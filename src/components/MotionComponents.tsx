a7de473c1a27510464e9408426898d75111d7c79
calebAnzy <caleb1475@gmail.com>
2025-10-05 18:56:03 +0100

    pusing again

--- src/components/MotionComponents.tsx
+++ src/components/MotionComponents.tsx
@@ -1,6 +1,6 @@
-"use client"
-import { motion } from "motion/react";
+// "use client"
+// import { motion } from "motion/react";
 
-export const MotionDiv = motion.div
-export const MotionSection = motion.section
-export const MotionArticle = motion.article
+// export const MotionDiv = motion.div
+// export const MotionSection = motion.section
+// export const MotionArticle = motion.article
