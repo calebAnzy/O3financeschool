0efd14a06020eded19b8dcb18ad3341fa54203cc
calebAnzy <caleb1475@gmail.com>
2025-10-05 18:59:28 +0100

    pushing again

--- src/components/MotionComponents.tsx
+++
@@ -1,6 +0,0 @@
-// "use client"
-// import { motion } from "motion/react";
-
-// export const MotionDiv = motion.div
-// export const MotionSection = motion.section
-// export const MotionArticle = motion.article
